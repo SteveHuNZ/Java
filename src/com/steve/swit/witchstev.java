e8da70028a37fb451604fb7409967d50d14feb61
stevehu <stevehuinnz@gmail.com>
2022-10-31 09:03:12 +1300

    monday1

---
+++ src/com/steve/swit/witchstev.java
@@ -0,0 +1,29 @@
+package com.steve.swit;
+
+public class witchstev {
+    public static void main(String[] args) {
+        String weekday = "zhousan";
+
+        switch (weekday){
+            case "zhouyi":
+                System.out.println("haha monday");
+                break;
+
+            case "zhouer":
+                System.out.println("haha tuesday");
+                break;
+
+
+
+            case "zhousan":
+                System.out.println("haha wednesday");
+                break;
+
+            default:
+                System.out.println("there are some worng");
+
+
+
+        }
+    }
+}
