c761373ba26c1f3422bf422ce1d9a1bdc1630d85
stevehu <stevehuinnz@gmail.com>
2022-11-25 23:32:28 +1300

    account

--- src/com/steve/atm/ATMSystem.java
+++ src/com/steve/atm/ATMSystem.java
@@ -1,6 +1,7 @@
 package com.steve.atm;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class ATMSystem {
@@ -11,7 +12,7 @@ public class ATMSystem {
         while (true) {
             System.out.println("=============ATM系统==============");
             System.out.println("1,账户登录");
-            System.out.println("1,账户开户");
+            System.out.println("2,账户开户");
 
             System.out.println("请您选择操作");
             int command = sc.nextInt();
@@ -25,6 +26,7 @@ public class ATMSystem {
                     break;
                 default:
                     System.out.println("您输入的命令不存在");
+                    
             }
         }
 
@@ -63,6 +65,52 @@ public class ATMSystem {
        account.setQuotaMoney(quotaMoney);
 
 
+//       创建一个８位数的账户号码且与其他不重复
+        String cardId = (String) getRandomCardId(accounts);
+        account.setCardId(cardId);
+
+
         //3 把账户对象添加到账户集合中去
+        accounts.add(account);
+        System.out.println(userName+"you sigin up successfully ,your car number is :"+ cardId);
     }
+
+    /**
+     * generate 8 numbers which different from other account numbers.
+     * @return
+     */
+
+    private static Object getRandomCardId(ArrayList<Account> accounts) {
+        while (true) {
+
+            Random r = new Random();
+            String cardId = "";
+
+            for(int i = 0;i<8;i++){
+                cardId += r.nextInt(10);
+
+
+            }
+
+            //根据这个卡号去查询账户对象
+            Account acc = getAccountByCardId(cardId,accounts);
+            if(acc == null){return cardId;}
+        }
+
+    }
+
+    /**
+     *search an account depends on the card number
+     * @param cardId    account number
+     * @param accounts  all accounts in the Array
+     * @return
+     */
+    private static Account getAccountByCardId(String cardId,ArrayList<Account> accounts){
+        for (int i = 0; i < accounts.size(); i++) {
+            Account acc = accounts.get (i);
+            if(acc.getCardId().equals(cardId)){return acc; }
+
+        }
+        return  null;
+    };
 }
