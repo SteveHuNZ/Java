20d12d80db3c006dba2cc1482f4ef699430fbfb4
stevehu <stevehuinnz@gmail.com>
2022-12-07 22:55:40 +1300

    static

---
+++ opp/src/User.java
@@ -0,0 +1,3 @@
+public class User {
+    public static int onlineNumber = 161;
+}
---
+++ opp/src/staticDemo.java
@@ -0,0 +1,5 @@
+public class staticDemo {
+    public static void main(String[] args){
+        System.out.println(User.onlineNumber);
+    }
+}
