b8303b9e9fc366d7a2e96867fb1f5ed2f8a331e4
stevehu <stevehuinnz@gmail.com>
2022-12-13 23:47:12 +1300

    tool

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name;
+    public static int getMax(int age1, int age2){
+        return age1>age2 ?age1:age2;
+    }
+    public  void study(){
+        System.out.println(name + "learning hard and growing fast ");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Student.getMax(10,3));
+        System.out.println(getMax(10,32));
+
+        Student s = new Student();
+        s.name = "kaitlyn";
+        s.study();
+    }
+}
+public static void start(android.content.Context context) {
+    android.content.Intent starter = new Intent(context, Student.class);
+    starter.putExtra();
+    context.startActivity(starter);
+}
