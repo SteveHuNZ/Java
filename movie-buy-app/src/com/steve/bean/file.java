53750365c0afe9d1a7c46f8b44935ef498b84d4a
stevehu <stevehuinnz@gmail.com>
2023-03-03 22:53:04 +1300

    dfds

---
+++ movie-buy-app/src/com/steve/bean/file.java
@@ -0,0 +1,15 @@
+package com.steve.bean;
+
+import java.io.File;
+
+public class file {
+    public static void main(String[] args) {
+        File f = new File("C:\\Users\\Administrator\\OneDrive\\Desktop\\Snipaste_2023-02-13_11-11-24.jpg");
+        long size = f.length();
+        System.out.println(size);
+    }
+}
+
+    public static void main(String[] args) {
+
+    }
